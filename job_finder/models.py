c17130ddb1e333faa50ccff2f1a1320bba6b59f1
Adil <subankulov_a@iuca.kg>
2024-05-20 14:48:19 +0600

    Detail view

--- job_finder/models.py
+++ job_finder/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Job(models.Model):
     title = models.CharField(max_length=50)
     salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -10,6 +11,7 @@ class Job(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+
 class Resume(models.Model):
     urls = models.URLField(blank=True, null=True)
     title = models.CharField(max_length=50)
@@ -18,11 +20,13 @@ class Resume(models.Model):
     Address = models.CharField(max_length=50)
     birthday = models.DateField()
     # models.PhoneNumberField(_(""))
-    description = models.TextField()   
+    description = models.TextField()
+
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
 
     def __str__(self):
         return self.title
--- job_finder/views.py
+++ job_finder/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job, Resume
 
-def baseView(request):
-    return render(request, 'job_finder/base_view.html', {'jobs': Job})
+def job_list(request):
+    jobs = Job.objects.all
+    return render(request, 'job_finder/job_list.html', {'jobs': jobs})
+
+def job_detail(request, pk):
+    jobs = get_object_or_404(Job, pk=pk)
+    return render(request, 'job_finder/job_detail.html', {'jobs': jobs})
